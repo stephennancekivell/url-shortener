313da77d7614cb61e4f51a6df28ad5ee089552f3
Stephen Nancekivell <stephen@redballoon.com.au>
2014-04-25 13:08:47 +1000

    Revert "remove unnecessary deps"
    This reverts commit 17095196c4a75062248a2618673a771deae407a3.

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
+#web: java $JVM_OPTS -cp target/url-shortener-standalone.jar clojure.main -m url-shortener.handler $PORT
+
 web: lein run -m url-shortener.handler $PORT
--- project.clj
+++ project.clj
@@ -2,9 +2,11 @@
   :description "simple url shortener"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
+                 [ring/ring-jetty-adapter "1.1.6"]
                  [com.taoensso/carmine "2.4.5"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler url-shortener.handler/app}
+  :uberjar-name "url-shortener-standalone.jar"
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
